d86e6a5f67c4c7f9d927e0979e98f40da43ad10c
Kelly Dwan <ryelle@users.noreply.github.com>
2020-09-28 18:55:48 -0400

    Set up environment

---
+++ block-pattern-creator.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Block Pattern Creator
+ * Description: Create block patterns on the frontend of a site.
+ * Version: 1.0.0
+ * Requires at least: 5.5
+ * Author: WordPress Meta Team
+ * Text Domain: block-pattern-creator
+ * License: GPL v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+namespace Block_Plugin_Creator;
