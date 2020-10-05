8a2eb44e7f165f7a1839dbaf9fafcb4a3ebad235
Kelly Dwan <ryelle@users.noreply.github.com>
2020-10-05 14:00:28 -0400

    Add a Settings tab for controlling pattern settings

--- src/components/inspector/index.js
+++ src/components/inspector/index.js
@@ -7,13 +7,32 @@ import { BlockInspector } from '@wordpress/block-editor';
 /**
  * Internal dependencies
  */
+import Settings from './settings';
 import './style.css';
 
 export default function Inspector() {
 	return (
 		<div className="block-pattern-creator__inspector">
-
-			<BlockInspector />
+			<TabPanel
+				className="block-pattern-creator__inspector-tab-panel"
+				tabs={ [
+					{
+						name: 'document',
+						title: 'Settings',
+					},
+					{
+						name: 'block',
+						title: 'Block',
+					},
+				] }
+			>
+				{ ( tab ) => (
+					<>
+						{ 'block' === tab.name && <BlockInspector /> }
+						{ 'document' === tab.name && <Settings /> }
+					</>
+				) }
+			</TabPanel>
 		</div>
 	);
 }
